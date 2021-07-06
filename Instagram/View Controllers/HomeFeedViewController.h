363bbc8bf03c5afab82b9814db8c4ae2fad78d5b
Samantha Burak <samantha.burak@gmail.com>
2021-07-06 18:25:41 -0400

    Added login and signup

---
+++ Instagram/View Controllers/HomeFeedViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeFeedViewController.h
+//  Instagram
+//
+//  Created by samanthaburak on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeFeedViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Instagram/View Controllers/LoginViewController.m
+++ Instagram/View Controllers/LoginViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "LoginViewController.h"
+#import <Parse/Parse.h>
 
 @interface LoginViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
@@ -24,10 +25,51 @@
 }
 
 - (IBAction)didTapLogin:(id)sender {
+    [self loginUser];
+}
+
+- (void)loginUser {
+    NSString *username = self.usernameTextField.text;
+    NSString *password = self.passwordTextField.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            
+            // display view controller that needs to shown after successful login
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
 }
 
 - (IBAction)didTapSignUp:(id)sender {
+    [self registerUser];
 }
+
+- (void)registerUser {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameTextField.text;
+    newUser.password = self.passwordTextField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
+}
+
+
 
 /*
 #pragma mark - Navigation
