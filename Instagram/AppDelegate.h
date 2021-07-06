33586bc6327a5e12a575da68091736da8741fcbf
Samantha Burak <samantha.burak@gmail.com>
2021-07-06 17:00:23 -0400

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by samanthaburak on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
